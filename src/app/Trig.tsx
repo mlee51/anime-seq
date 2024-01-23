184ad289244745f4d1a33f4f3604aefe5cfea80b
Michael Lee <mleeteak13@gmail.com>
2024-01-23 00:52:11 -0500

    refactoring, layout changes, added shifting function, added ability to send midi cc without note

--- src/app/Trig.tsx
+++ src/app/Trig.tsx
@@ -1,11 +1,12 @@
 //@ts-nocheck
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import plusIcon from '../../public/icons/plus.svg'
 
-export default function Trig({ id, handleTrigs, handlePitch, trig, pitch, currentTrig, handleCC, cc }) {
+export default function Trig({ id, handleTrigs, handlePitch, trig, pitch, currentTrig, handleCC, cc, handleCCOn }) {
     const [isDragging, setIsDragging] = useState<false>(false);
     const [initialTouch, setInitialTouch] = useState<number>(0);
     const [hovering, setHovering] = useState<boolean>(false)
-    const [midicc, setMidiCC] = useState<boolean>(false)
 
     const handleMouseDown = (e) => {
         const rect = e.currentTarget.getBoundingClientRect();
@@ -20,9 +21,8 @@ export default function Trig({ id, handleTrigs, handlePitch, trig, pitch, curren
             const offsetX = e.clientX - initialTouch;
             const percentage = (offsetX / window.innerWidth) * window.innerWidth;
             let newVal = pitch + percentage;
-
             // Limit pitch to the range of 0 to 127
-            newVal = Math.min(52, Math.max(36, newVal));
+            newVal = Math.min(127, Math.max(0, newVal));
             handlePitch(id, newVal);
             console.log(percentage, newVal);
         }
@@ -40,6 +40,7 @@ export default function Trig({ id, handleTrigs, handlePitch, trig, pitch, curren
         setIsDragging(false);
     };
 
+
     useEffect(() => {
         // Add event listener when the component mounts
         window.addEventListener('mousemove', handleMouseMove);
@@ -52,26 +53,19 @@ export default function Trig({ id, handleTrigs, handlePitch, trig, pitch, curren
             window.removeEventListener('mouseup', handleMouseUp);
             window.removeEventListener('mouseleave', handleMouseLeave);
         };
-    }, [isDragging, initialTouch, pitch, handlePitch]); // Include dependencies in the dependency array
+    }, [isDragging, initialTouch, pitch, handlePitch]); 
 
     const handleWheel = (e) => {
-        // Adjust pitch based on the wheel delta
-        let newVal = pitch - e.deltaY / 100; // You can adjust the division factor for sensitivity
-
-        // Limit pitch to the range of 0 to 127
-        newVal = Math.min(52, Math.max(36, newVal));
+        let newVal = pitch - e.deltaY / 100; 
+        newVal = Math.min(127, Math.max(0, newVal));
         console.log(e.deltaY)
         handlePitch(id, newVal);
     };
 
     const handleCCWheel = (e) => {
-        // Adjust pitch based on the wheel delta
-        console.log(cc)
-        let newVal = cc - e.deltaY / 100; // You can adjust the division factor for sensitivity
-
-        // Limit pitch to the range of 0 to 127
+        let newVal = cc.val - e.deltaY / 100;
         newVal = Math.min(127, Math.max(0, newVal));
-        handleCC(id, newVal);
+        handleCC(id, { ...cc, val: newVal });
     };
 
     const calculateBackgroundColor = (pitch) => {
@@ -82,12 +76,12 @@ export default function Trig({ id, handleTrigs, handlePitch, trig, pitch, curren
 
     return (
         <>
-            <div className="flex flex-col"
+            <div className="flex flex-col mx-1"
                 onMouseOver={() => setHovering(true)}
                 onMouseLeave={() => setHovering(false)}>
                 <div
                     onClick={handleClick}
-                    className={(currentTrig === id ? 'bg-white' : trig ? 'bg-[#82d9b9]' : 'bg-black') + ' w-10 h-14 rounded-md'}>
+                    className={(currentTrig === id ? 'bg-white' : trig ? 'bg-amber-200' : 'bg-black') + ' w-10 h-14 rounded-md hover:bg-amber-100'}>
                 </div>
                 {trig && <div
                     onMouseDown={handleMouseDown}
@@ -96,20 +90,26 @@ export default function Trig({ id, handleTrigs, handlePitch, trig, pitch, curren
                     className="flex flex-col items-center justify-center select-none w-10 h-14  text-black font-extrabold rounded-md mt-2 hover:h-20">
                     {pitch}
                 </div>}
-                {midicc && <div
-                    onWheel={handleCCWheel}
-                    style={{ backgroundColor: calculateBackgroundColor(cc) }}
-                    className="flex flex-col items-center justify-center select-none w-10 h-14  text-black font-extrabold rounded-[15px] mt-2 hover:h-20">
-                    {cc}
+                {cc.on && <div
+                    style={{ backgroundColor: calculateBackgroundColor(cc.val) }}
+                    className='mt-2 rounded-b-[15px] rounded-t-sm'>
+                    <div
+                        onClick={() => { handleCCOn(id, !cc.on) }}
+                        className='text-sm text-black font-semibold text-center w-10 h-4 select-none hover:opacity-30'
+                        title="Delete"
+                    >
+                        127
+                    </div>
+                    <div
+                        onWheel={handleCCWheel}
+                        className="flex flex-col items-center justify-center select-none w-10 h-10  text-black font-extrabold   hover:h-12">
+                        {cc.val}
+                    </div>
                 </div>}
                 {hovering && <div
-                    onClick={()=>{setMidiCC(!midicc)}}
-                    style={{ backgroundColor: calculateBackgroundColor(pitch) }}
-                    className="w-10 h-10 flex flex-col items-center justify-center select-none  rounded-lg mt-2 p-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="black" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                    </svg>
-
+                    onClick={() => { handleCCOn(id, !cc.on) }}
+                    className="w-10 h-10 flex flex-col items-center justify-center select-none rounded-lg mt-2 p-4 bg-black opacity-50">
+                    <Image src={plusIcon} alt="pause"/>
                 </div>}
             </div>
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,68 @@
 //@ts-nocheck
 'use client'
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, Suspense } from "react";
+import Image from 'next/image';
 import Trig from "./Trig";
 import { WebMidi } from "webmidi";
-import Knob from "./Knob";
+import pauseIcon from '../../public/icons/pause.svg'
+import playIcon from '../../public/icons/play.svg'
+import leftIcon from '../../public/icons/leftarrow.svg'
+import rightIcon from '../../public/icons/rightarrow.svg'
 
 export default function Home() {
   const [channel, setChannel] = useState<any>(null);
   const [output, setOutput] = useState<any>(null);
   const [running, setRunning] = useState<boolean>(false);
-  const [trigs, setTrigs] = useState(Array(16).fill({ on: false, pitch: 36, cc: 60 }));
-  const [intervalId, setIntervalId] = useState<any>();
+  const [trigs, setTrigs] = useState(Array(16).fill({ on: false, pitch: 36, cc: { on: false, val: null } }));
   const [currentTrig, setCurrentTrig] = useState<number>(0)
   const trigsRef = useRef(trigs);
   const currentTrigRef = useRef(currentTrig)
+  const [bpm, setBpm] = useState<number>(120)
+  const loopTime = useRef(60000 / bpm / 4)
+
+  useEffect(() => {
+    WebMidi.enable(onEnabled)
+      .then(() => console.log("WebMidi enabled!"))
+      .catch((err) => alert(err))
+      .finally(() => {
+        setOutput(WebMidi.outputs[0])
+        setChannel(WebMidi.outputs[0].channels[1])
+      });
+  }, []);
+
+  useEffect(() => {
+    loopTime.current = 60000 / bpm / 4
+  }, [bpm])
+
   useEffect(() => {
     trigsRef.current = trigs;
   }, [trigs]);
+
+  useEffect(() => {
+    currentTrigRef.current = currentTrig
+    playNotes()
+  }, [currentTrigRef.current])
+
+  useEffect(() => {
+    let lightsInterval: NodeJS.Timeout
+    if (running) {
+      playNotes()
+      currentTrigRef.current = (currentTrigRef.current + 1) % 16
+      setCurrentTrig((prevCount) => (prevCount + 1) % 16)
+      lightsInterval = setInterval(() => {
+        currentTrigRef.current = (currentTrigRef.current + 1) % 16
+        setCurrentTrig((prevCount) => (prevCount + 1) % 16);
+      }, loopTime.current);
+    } else {
+      setCurrentTrig(0)
+      currentTrigRef.current = 0
+    }
+
+    return () => {
+      clearInterval(lightsInterval);
+    };
+  }, [running, bpm])
 
   const handleTrigs = (id: number) => {
     setTrigs((prevStates) => {
@@ -35,104 +80,114 @@ export default function Home() {
     });
   }
 
-  const handleCC = (id: number, e: string) => {
+  const handleCC = (id: number, e: any) => {
+    const ccVal = parseInt(e.val)
     setTrigs((prevStates) => {
       const newTrigStates = [...prevStates];
-      newTrigStates[id] = { ...newTrigStates[id], cc: parseInt(e) }
+      newTrigStates[id] = { ...newTrigStates[id], cc: { ...newTrigStates[id]?.cc, val: e.val } }
+      return newTrigStates;
+    });
+    if (!running) {
+      channel.sendControlChange(127, ccVal)
+    }
+  }
+
+  const handleCCOn = (id: number, e: boolean) => {
+    setTrigs((prevStates) => {
+      const newTrigStates = [...prevStates];
+      if (e) newTrigStates[id] = { ...newTrigStates[id], cc: { ...newTrigStates[id]?.cc, on: e } }
+      else newTrigStates[id] = { ...newTrigStates[id], cc: { ...newTrigStates[id]?.cc, on: e, val: null } }
       return newTrigStates;
     });
   }
+  const handleBpmWheel = (e) => {
+    let newVal = bpm - e.deltaY / 100;
+    newVal = Math.min(300, Math.max(1, newVal));
+    setBpm(newVal)
+  };
+
+  const shiftTrigsLeft = () => {
+    setTrigs((prevStates) => {
+      const newTrigStates = [...prevStates];
+      const newArray = [];
+
+      for (let i = 1; i < newTrigStates.length; i++) {
+        newArray.push(newTrigStates[i]);
+      }
+      newArray.push(newTrigStates[0]);
+      return newArray;
+    });
+  };
+
+  const shiftTrigsRight = () => {
+    setTrigs((prevStates) => {
+      const newTrigStates = [...prevStates];
+      const newArray = [];
+      const length = newTrigStates.length
+      newArray.push(newTrigStates[length - 1]);
+      for (let i = 0; i < length - 1; i++) {
+        newArray.push(newTrigStates[i]);
+      }
+      return newArray;
+    });
+  };
 
   const onEnabled = () => {
     WebMidi.inputs.forEach((input) => console.log(input.manufacturer, input.name));
     WebMidi.outputs.forEach((output) => console.log(output.manufacturer, output.name));
   };
 
-  // const playNotes = () => {
-  //   trigsRef.current.forEach((trig, index) => {
-  //     if (trig.on) {
-  //       const pitch = trig.pitch
-  //       channel.playNote(pitch, { time: `+${index * 100}`, duration: 100 });
-  //     }
-  //   });
-  // };
-
   const playNotes = () => {
     const { pitch, on, cc } = trigsRef.current[currentTrigRef.current]
-    if(on){
-      channel.sendControlChange(127, cc).playNote(pitch, { time: WebMidi.time, duration: 100 })
+    if (on) {
+      channel.playNote(pitch, { time: WebMidi.time, duration: loopTime.current })
+    }
+    if (cc.val != null) {
+      channel.sendControlChange(127, cc.val)
     }
   }
 
-  const handleRunning = () => {
-    setRunning(!running)
-  }
-
-  useEffect(()=>{
-    currentTrigRef.current = currentTrig
-    playNotes()
-  },[currentTrigRef.current])
-
-  useEffect(() => {
-    let lightsInterval: NodeJS.Timeout
-    if (running) {
-      playNotes()
-      currentTrigRef.current = (currentTrigRef.current + 1) % 16
-      setCurrentTrig((prevCount) => (prevCount + 1) % 16)
-      lightsInterval = setInterval(() => {
-        currentTrigRef.current = (currentTrigRef.current + 1) % 16
-        setCurrentTrig((prevCount) => (prevCount + 1) % 16);
-      }, 100);
-      //const id: NodeJS.Timeout = setInterval(playNotes, 100);
-      //setIntervalId(id);
-    } else {
-      //clearInterval(intervalId);
-      //setIntervalId(null);
-      setCurrentTrig(0)
-      currentTrigRef.current = 0
+  const handleRunning = (e) => {
+    if (e.target === e.currentTarget) {
+      setRunning(!running)
     }
-
-    return () => {
-      clearInterval(lightsInterval);
-    };
-  }, [running])
-
-  useEffect(() => {
-
-    WebMidi.enable(onEnabled)
-      .then(() => console.log("WebMidi enabled!"))
-      .catch((err) => alert(err))
-      .finally(() => {
-        setOutput(WebMidi.outputs[0])
-        setChannel(WebMidi.outputs[0].channels[1]
-        )
-      });
-  }, []);
+  }
 
   return (
-    <>
-      <div className="fixed font-extrabold text-3xl select-none right-1/4 top-1/4">
-        <button onClick={handleRunning}>
-          {running ? 'Stop' : 'Play'}
-        </button>
+    <Suspense>
+      <div className="fixed w-full font-extrabold text-3xl select-none inline">
+        <div className="fixed bottom-20 left-1/2 transform text-[2rem] -translate-x-1/2 -translate-y-1/2 opacity-10 pointer-events-none">
+          {running ? <Image width={200} src={pauseIcon} alt="pause" /> : <Image width={200} src={playIcon} alt="play" />}
+        </div>
+        <div
+          className="fixed top-10 left-1/2 transform text-[2rem] -translate-x-1/2 -translate-y-1/2 opacity-20 text-black"
+          onWheel={handleBpmWheel}>
+          {bpm}
+        </div>
       </div>
-      <div className="max-w-5xl mx-auto">
-        <main className="flex min-h-screen flex-row items-start justify-around pl-24 pr-24 pt-[24rem] bg-[#10191f]">
-          {Array.from({ length: 16 }, (_, index) => (
+      <div>
+        <main onClick={handleRunning} className="flex flex-row items-start justify-around pl-24 pr-24 pt-[12rem] bg-[#10191f] h-screen">{/*bg-[#10191f]*/}
+          <button onClick={shiftTrigsLeft} className="h-14 mr-4">
+          <Image src={leftIcon} alt="Shift Left" />
+          </button>
+          {trigs.map((_, index) => (
             <Trig
               key={index}
               handleTrigs={handleTrigs}
               handlePitch={handlePitch}
               handleCC={handleCC}
+              handleCCOn={handleCCOn}
               pitch={trigs[index].pitch}
               cc={trigs[index].cc}
               id={index} trig={trigs[index].on}
               currentTrig={currentTrig}
             />
           ))}
+          <button onClick={shiftTrigsRight} className="h-14 ml-4">
+            <Image src={rightIcon} alt="Shift Right" />
+          </button>
         </main >
       </div>
-      {/* <Knob output={output}/> */}
-    </>
+    </Suspense>
   );
 }
